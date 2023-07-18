ecc01425f31362d06d0236ccec60f559855ccdad
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2023-07-18 15:26:29 -0300

    Criei o crud básico para funcionários

--- src/funcionario/dto/create-funcionario.dto.ts
+++ src/funcionario/dto/create-funcionario.dto.ts
@@ -1,1 +1,12 @@
-export class CreateFuncionarioDto {}
+import { MaxLength, MinLength } from 'class-validator';
+
+export class CreateFuncionarioDto {
+  name: string;
+  @MinLength(8, {
+    message: 'This password is too short. Minimal Length is 8.',
+  })
+  @MaxLength(28, {
+    message: 'This password is too long. Maximum Length is 28.',
+  })
+  password: string;
+}
--- src/funcionario/funcionario.service.ts
+++ src/funcionario/funcionario.service.ts
@@ -1,26 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateFuncionarioDto } from './dto/create-funcionario.dto';
 import { UpdateFuncionarioDto } from './dto/update-funcionario.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Funcionario } from './entities/funcionario.entity';
 
 @Injectable()
 export class FuncionarioService {
+  // Injeta a classe de Funcionário dentro deste arquivo para conseguirmos acessar todas suas funcionalidades
+  constructor(
+    @InjectRepository(Funcionario)
+    private funcionarioRepository: Repository<Funcionario>,
+  ) {}
   create(createFuncionarioDto: CreateFuncionarioDto) {
-    return 'This action adds a new funcionario';
+    const newFuncionario = this.funcionarioRepository.create({
+      ...createFuncionarioDto,
+      createdAt: new Date(),
+    });
+    this.funcionarioRepository.save(newFuncionario);
   }
 
   findAll() {
-    return `This action returns all funcionario`;
+    return this.funcionarioRepository.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} funcionario`;
+    return this.funcionarioRepository.findOneBy({ id });
   }
 
   update(id: number, updateFuncionarioDto: UpdateFuncionarioDto) {
-    return `This action updates a #${id} funcionario`;
+    this.funcionarioRepository.update({ id }, { ...updateFuncionarioDto });
   }
 
   remove(id: number) {
-    return `This action removes a #${id} funcionario`;
+    return this.funcionarioRepository.delete({ id });
   }
 }
